066fd833b619ad3b619a681557beba0f62ea508d
sagar04-allprojectagent <62507649+sagar04-allprojectagent@users.noreply.github.com>
2020-04-14 11:29:55 +0530

    multiplication of 2 numbers in c language
    learn c language how its work...

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <conio.h>
+#include <stdio.h>
+
+int main()
+{
+   int x,y;
+   
+   printf("enter number:\n", x);
+   
+   scanf("%d", &x);
+   
+   printf("enter number:\n", y);
+   
+   scanf("%d", &y);
+   
+   printf("ans is:%d\n", x*y);
+   
+   return 0;
+    
+}
